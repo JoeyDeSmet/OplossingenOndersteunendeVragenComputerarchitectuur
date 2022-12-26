f47bb2a98445106371b71d9d4490bd4abfba7b6a
Joey <joey@de-smet.vlaanderen>
2022-12-26 10:59:51 +0100

    Add repo link

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: 'https://github.com/JoeyDeSmet/OplossingenOndersteunendeVragenComputerarchitectuur',
     editLinks: false,
     docsDir: '',
     editLinkText: '',
